162a1d752a1900be837ad94304ebae89145bd1da
张佳珺 <1447191556@qq.com>
2022-08-21 20:54:34 +0800

    aqs

---
+++ src/main/java/com/taikang/grpcontract/GrpcontractApplication.java
@@ -0,0 +1,13 @@
+package com.taikang.grpcontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcontractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcontractApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taikang/grpcontract/GrpcontractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taikang.grpcontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcontractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
